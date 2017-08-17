193b7d35975e30f6480cc2768e51dd24c7cce9b7
grapebaba <281165273@qq.com>
2017-08-17 19:06:46 +0800

    fix

--- scripts/script.sh
+++ scripts/script.sh
@@ -224,6 +224,7 @@ instantiateChaincode 0
 #echo "Instantiating chaincode on org2/peer2..."
 #instantiateChaincode 2
 
+sleep 100
 #Invoke on chaincode on Peer0/Org1
 echo "Sending invoke transaction on org1/peer0..."
 chaincodeInvoke 0
